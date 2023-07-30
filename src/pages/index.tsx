03e275e0555659a46288c848e3e43d84f1128092
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-07-30 10:18:34 +0600

    index page modified

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,66 +7,43 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main
-    className={`flex min-h-screen flex-col items-center justify-between md:p-24 ${inter.className}`}
-  >
-    <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Hello There....!&nbsp;
-        <code className="font-mono font-bold">Welcome to</code>
-      </p>
-      {/* <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-      </div> */}
-    </div>
-
-    <div className="relative md:flex justify-center place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[200px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] mt-0">
-      <Image
-        src="/logo/PTECCC-LOGO.png"
-        alt='Ptec career club logo'
-        height={200}
-        width={200}
-      />
-      <h1 className='md:text-6xl text-3xl font-bold animate-pulse'>PTEC Career Club</h1>
-    </div>
-
-    <div className="mb-12 pb-16 md:pb-0 grid text-center lg:mb-0 lg:grid-cols-1">
-      <Link
-        href="/aboutus"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        // target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-          Let&apos;s Get Started{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-          &rarr;
-          </span>
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Find in-depth information about PTEC Career Club.
+      className={`flex min-h-screen flex-col items-center justify-between md:p-24 ${inter.className}`}
+    >
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Hello There....!&nbsp;
+          <code className="font-mono font-bold">Welcome to</code>
         </p>
-      </Link>
+      </div>
 
-      {/* <a
-        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2 className={`mb-3 text-2xl font-semibold`}>
-          Learn{' '}
-          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            -&gt;
-          </span>
-        </h2>
-        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          Learn about Next.js in an interactive course with&nbsp;quizzes!
-        </p>
-      </a> */}
+      <div className="relative md:flex justify-center place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[200px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px] mt-0">
+        <Image
+          src="/logo/PTECCC-LOGO.png"
+          alt='Ptec career club logo'
+          height={200}
+          width={200}
+        />
+        <h1 className='md:text-6xl text-3xl font-bold animate-pulse'>PTEC Career Club</h1>
+      </div>
 
-
-
-
-    </div>
-  </main>
+      <div className="mb-12 pb-16 md:pb-0 grid text-center lg:mb-0 lg:grid-cols-1">
+        <Link
+          href="/aboutus"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          // target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Let&apos;s Get Started{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              &rarr;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about PTEC Career Club.
+          </p>
+        </Link>
+      </div>
+    </main>
   )
 }
