a1edc4975b576ae4d58c2421bb3b04112c6a7f46
juniorashraf02 <shuvo.istiak11@gmail.com>
2023-08-06 19:29:43 +0600

    some changes made

--- src/components/Home/Carousel/Carousel.tsx
+++ src/components/Home/Carousel/Carousel.tsx
@@ -1,33 +1,60 @@
 import Image from "next/image";
 
-
-
 export const Carousel = () => {
-
-    return (
-        <div className="w-screen">
-            <div className="carousel py-10 h-1/2 bg-white">
-                <div className="carousel-item gap-5">
-                    <Image width={500} height={500} className="rounded-box md:px-5 px-2" src="/photos/home/carousel/asifSir.png" alt="Burger" />
-                </div>
-                <div className="carousel-item gap-5">
-                    <Image width={500} height={500} className="rounded-box md:px-5 px-2" src="/photos/home/carousel/alaminBro.png" alt="Burger" />
-                </div>
-                <div className="carousel-item gap-5">
-                    <Image width={500} height={500} className="rounded-box md:px-5 px-2" src="/photos/home/carousel/mendi.png" alt="Burger" />
-                </div>
-                <div className="carousel-item gap-5">
-                    <Image width={500} height={500} className="rounded-box md:px-5 px-2" src="/photos/home/carousel/ezu.png" alt="Burger" />
-                </div>
-
-
-
-
-
-            </div>
+  return (
+    <div className="w-screen">
+      <div className="carousel py-10 h-1/2 bg-white">
+        <div className="carousel-item gap-5">
+          <div className="image-container">
+            <Image
+              layout="intrinsic"
+              className="rounded-box md:px-5 px-2"
+              src="/photos/home/carousel/asifSir.png"
+              alt="Burger"
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
-    )
-
-}
+        <div className="carousel-item gap-5">
+          <div className="image-container">
+            <Image
+              layout="intrinsic"
+              className="rounded-box md:px-5 px-2"
+              src="/photos/home/carousel/alaminBro.png"
+              alt="Burger"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+        <div className="carousel-item gap-5">
+          <div className="image-container">
+            <Image
+              layout="intrinsic"
+              className="rounded-box md:px-5 px-2"
+              src="/photos/home/carousel/mendi.png"
+              alt="Burger"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+        <div className="carousel-item gap-5">
+          <div className="image-container">
+            <Image
+              layout="intrinsic"
+              className="rounded-box md:px-5 px-2"
+              src="/photos/home/carousel/ezu.png"
+              alt="Burger"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carousel;
